a8865ac5123651ebfec25287c59213a9cb9699c2
ColourLaserPrinterWithDuplexPrint <79779415+ColourLaserPrinterWithDuplexPrint@users.noreply.github.com>
2021-03-02 20:02:07 +1100

    Update libraryChecker.py

--- Input Sanitation/libraryChecker/libraryChecker.py
+++ Input Sanitation/libraryChecker/libraryChecker.py
@@ -4,37 +4,49 @@ def main():
     try:
         print("\nA simple Python app to check if library exists!")
         ip = input("IP To Bind On [default 127.0.0.1] : ")
+        #Defaults to 127.0.0.1 (localhost)
         if ip == "":
             ip == '127.0.0.1'
         port = input("Port To Bind On : ")
+        #Initialises socket type
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Checks if port is a valid integer
         try:
             port = int(port)
         except:
             print("Invalid Port Number")
         print("Binding...")
+        #Binds on the specified port and IP
         s.bind((ip, port))
         print("Binded Successfully, Starting Listener")
+        #Starts Listening On Port
         s.listen(0)
         while True:
             print("Waiting For Connection")
+            #Waits for a connection and saves the connection object to 'conn' and the connected IP/Port to 'addr'
             conn,addr = s.accept()
             print("Connection Recieved From " + str(addr))
+            #Receives sent data
             data = conn.recv(1024)
+            #Decodes data and saves it to a variable called 'library'
             library = data.decode()
             try:
                 #Here is the exploit.
-                #The Python program executes a query from the user without checking the contents of it, leading to remote code execution (RCE)
+                #To check if the library is valid, the Python program executes a query from the user without checking the contents of it, leading to remote code execution (RCE)
                 exec("import " + library)
+                #If this hasn't thrown an error, the library does exist
                 conn.send(b"[+] Library Does Exist")
             except Exception as e:
+                #If the try statement threw an error while importing the library, it means that the library does not exist
                 print(e)
                 conn.send(b"[-] Library Does Not Exist")
+            #Shuts down and closes the connection
             conn.shutdown(socket.SHUT_RDWR)
             conn.close()
             print("Connection Closed Successfully\n")
     except Exception as e:
         print(e)
 
+#Runs forever
 while True:
     main()
