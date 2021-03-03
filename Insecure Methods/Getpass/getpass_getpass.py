c7ed5deca2023a2c5b3a4a92d642bfc15b8d5062
ColourLaserPrinterWithDuplexPrint <79779415+ColourLaserPrinterWithDuplexPrint@users.noreply.github.com>
2021-03-03 19:30:54 +1100

    Update getpass_getpass.py

--- Insecure Methods/Getpass/getpass_getpass.py
+++ Insecure Methods/Getpass/getpass_getpass.py
@@ -1,1 +1,19 @@
 import getpass
+user = getpass.getuser()
+if user == "root":
+    print("[+] You Are Authorized. The Password Is abc123")
+else:
+    print("[-] You Are Not Authorized.")
+    exit()
+input("Press Enter To Exit >> ")
+
+# The problem with 'getpass.getuser()' is that it searches for environment variables to get the username of the current logged in user, and environment variables can be manipulated.
+#
+# getpass.getuser() checks the following environment variables : LOGNAME, USER, LNAME and USERNAME
+#
+# On Unix/Linux, you can use the following code to bypass this program :
+#           export LOGNAME='root'
+#           export USER='root'
+#           export LNAME='root'
+#           export USERNAME='root'
+
