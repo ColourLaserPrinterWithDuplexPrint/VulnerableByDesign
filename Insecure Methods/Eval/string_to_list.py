a865b198ee56fb6b3f62f1d2e40d3c3ef1a73789
ColourLaserPrinterWithDuplexPrint <79779415+ColourLaserPrinterWithDuplexPrint@users.noreply.github.com>
2021-03-17 21:14:56 +1100

    Update string_to_list.py

--- Insecure Methods/Eval/string_to_list.py
+++ Insecure Methods/Eval/string_to_list.py
@@ -11,9 +11,20 @@ while True:
     print("Failed to convert string to list...")
   print("\n")
 
-# The python 'eval' statement executes a variable. It can be used to convert a string to a list.
+# The python 'eval' statement executes a variable. It is sometimes used to convert a string to a list.
 #
-# However, you could also enter 'print("abc")' as the list and your query will be executed, which means that you could execute code through 'eval'. The limits to eval RCE is that eval can't execute very sophisticated queries, unlike 'exec'.
+# In this example, you could enter a Python array and it would iterate through the array which you provided. However, you could also enter 'print("abc")' into the input and your query will be executed, which means that you could execute code through 'eval'. The limits to eval RCE is that eval can't execute very sophisticated queries without throwing errors, unlike 'exec'.
 #
-# A better option to convert a string to list would be to use 'ast.literal_eval(string)'. This will throw an error if the statement is not a list, but the unwanted code will not be executed.
+#
+# Sophisticated RCE with eval can be achieved through using an 'exec' statement in your initial RCE. For example : 
+#
+# eval('''exec("import subprocess; print(subprocess.check_output('cat /etc/shadow', shell=True))")''')
+#
+# - As opposed to :
+# eval("import subprocess; print(subprocess.check_output('cat /etc/shadow')")
+#
+# Which would throw an error.
+#
+#
+# A better option to convert a string to list would be to use 'ast.literal_eval(string)'. This will throw an error if the statement is not a valid data type, but the unwanted code will not be executed.
 
